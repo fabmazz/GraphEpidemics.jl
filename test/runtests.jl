6c9498723ee2e169aa377bf5b21d6f6e1947ca13
Fabio Mazza <fab4mazz@gmail.com>
2025-10-09 16:15:26 +0200

    add SEIR Het test

--- test/runtests.jl
+++ test/runtests.jl
@@ -68,3 +68,23 @@ end
 
     @test datas[2][end] > 10
 end
+
+@testset "SEIR_heterog" begin
+    N=100
+    d=10
+    G=random_regular_graph(N,d, seed=4)
+
+    DEG=d
+
+    rng = Xoshiro(2)
+    gam = 1/10
+    bet = beta_R0(1.8,DEG,gam)
+    a1 = rand(rng,N)*0.3 .+0.7
+    s1 = rand(rng,N)*0.5 .+0.5
+    model = SEIRHetModel(bet,a1,s1,gam*2,gam )
+
+    sd,cc = run_seir_fast(G,model,-1,rng, sample(rng,1:N,5,replace=false),delays_types=Int32,dtype=Float32)
+    cpl = stack(cc)
+
+    @test cpl[:,end] == [83,0,0,17]
+end
